7ad823b516a0c59a83aa1d0587a6db8c5338c060
yarimar-labo <ydiazm1990@gmail.com>
2020-07-29 18:43:56 -0400

    Se muestra informacion de Href, texto y file

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let fs = require("fs"); // Módulo  de sistema de archivos
 let regEx = /(((https?:\/\/)|(http?:\/\/)|(www\.))[^\s\n)]+)(?=\))/g; // Patrón para encontrar links
+let expectMDLink = /\[([^\]]*)\]\(([^)]*)\)/g;
+let filesMDLinks = []; //Array para guardar la inf (URL, Text, File)
 let dir = require("node-dir"); // Lee Archivos y subdirectorios
 
 // Extrae lo link de cada archivo recibido
@@ -10,7 +12,18 @@ const readFile = (files) => {
         console.error(e);
       } else {
         const linksFound = file.match(regEx);
-        console.log(`links encontrados en ${element}` , linksFound);
+        const textLinks = file.match(expectMDLink)
+        .map((v) => v.split('](')[0].slice(1));
+        linksFound.forEach((link, i) =>
+        filesMDLinks.push({
+          href: link,
+          text: textLinks[i],
+          file: element,
+        })
+      );
+      //  console.log('SOY EL file', textLinks)
+     //   console.log(`links encontrados en ${element}` , linksFound);
+     console.log('Prueba', filesMDLinks)
       }
     });
   });
